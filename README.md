0ce6f71f25d6031128604275609dd048b0d6cbf0
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-01-20 14:20:07 -0300

    Projeto Java Challenge Conversor de Moedas

---
+++ README.md
@@ -0,0 +1,16 @@
+### 🪙 Currency Converter 🪙
+## 📖 Sobre
+Neste projeto, busca-se uma forma de converter a moeda de um país para a de outro de maneira fácil e rápida. Ele utiliza uma API gratuita de conversão de moedas, na qual é possível informar a moeda base e a moeda alvo para a qual se deseja realizar a conversão.
+
+### 📜 Detalhes
+#### 🪛 Menu
+Com um menu fácil de entender e usar, o usuário pode escolher entre opções pré-definidas ou selecionar suas próprias moedas. Além disso, há a possibilidade de converter e monitorar suas conversões de forma prática e eficiente.
+
+<tr> <img src="./src/img/menu.png">
+⚙️ Funcionalidades
+<p><strong>Opção pré-definida</strong></p>
+<img src="src/img/option one.gif"> 
+<p><strong>Histórico de conversões</strong></p> 
+<img src="./src/img/optionsevengif.gif"> 
+<p><strong>Escolher suas próprias moedas</strong></p> 
+<img src="./src/img/option conversion.gif">
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,83 @@
+import br.com.metodoscurrency.metodos;
+
+import java.io.IOException;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int opcao = 0;
+        double ammount = 0;
+
+        Scanner leitura = new Scanner(System.in);
+        metodos moeda = new metodos();
+
+
+        moeda.menuOne();
+        while (opcao != 10) {
+            moeda.menu();
+            opcao = leitura.nextInt();
+            switch (opcao) {
+                case 1:
+                    moeda.mensagem();
+                    ammount = leitura.nextDouble();
+                    moeda.getConversor("USD", "ARS", ammount);
+                    moeda.menuTwo();
+                    break;
+                case 2:
+                    moeda.mensagem();
+                    ammount = leitura.nextDouble();
+                    moeda.getConversor("ARS", "USD", ammount);
+                    moeda.menuTwo();
+                    break;
+                case 3:
+                    moeda.mensagem();
+                    ammount = leitura.nextDouble();
+                    moeda.getConversor("USD", "BRL", ammount);
+                    moeda.menuTwo();
+                    break;
+                case 4:
+                    moeda.mensagem();
+                    ammount = leitura.nextDouble();
+                    moeda.getConversor("BRL", "USD", ammount);
+                    moeda.menuTwo();
+                    break;
+                case 5:
+                    moeda.mensagem();
+                    ammount = leitura.nextDouble();
+                    moeda.getConversor("USD", "COP", ammount);
+                    moeda.menuTwo();
+                    break;
+                case 6:
+                    moeda.mensagem();
+                    ammount = leitura.nextDouble();
+                    moeda.getConversor("COP", "USD", ammount);
+                    moeda.menuTwo();
+                    break;
+                case 7:
+                    moeda.ListOfMoney();
+                    break;
+                case 8:
+                    System.out.println("Digite o Código do País Exp: [USD] para converter ");
+                    String paisOne = leitura.next();
+                    System.out.println("Digite o Código do País para qual sera convertido Exp: [BRL]");
+                    String paisTwo = leitura.next();
+                    System.out.println("Digite o valor para conversão");
+                    ammount = leitura.nextDouble();
+                    moeda.getConversor(paisOne, paisTwo, ammount);
+                    moeda.menuTwo();
+                    break;
+                case 9:
+                    moeda.ListdeCodigo();
+                    break;
+                case 10:
+                    System.out.println("************************************");
+                    System.out.println("Encerrando.......");
+                    System.out.println("************************************");
+                    break;
+                default:
+                    System.out.println("Escolha uma opção valida\n");
+                    break;
+            }
+
+        }
+        }
 }
